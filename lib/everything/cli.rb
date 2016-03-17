53186f7e9e4982c9545850b52096e9d9393ccbaf
Kyle Tolle <kyle@nullsix.com>
2016-03-16 22:40:06 -0600

    Create a new piece with everything-core

--- lib/everything/cli.rb
+++ lib/everything/cli.rb
@@ -3,45 +3,34 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default)
 
-require 'fileutils'
-
-require 'active_support'
 require 'active_support/core_ext/string'
 
 module Everything
   class CLI < Thor
     desc "new PIECE_NAME", "Create a new everything piece, in your current directory, with the given name, which must be in spinal-case."
     def new(piece_name)
-      piece_path_to_create = piece_path(piece_name)
-      Dir.mkdir(piece_path_to_create)
+      path = piece_path(piece_name)
+      piece = Everything::Piece.new(path)
 
-      content_file      = 'index.md'
-      content_file_path = File.join(piece_path_to_create, content_file)
-      FileUtils.touch(content_file_path)
-
-      titleized_name = piece_name.gsub('-', ' ').titleize
-      markdown_header = "# #{titleized_name}"
-
+      titleized_name        = piece_name.gsub('-', ' ').titleize
+      markdown_header       = "# #{titleized_name}"
       default_markdown_body = <<MD
 #{markdown_header}
 
 
 
 MD
+      piece.raw_markdown    = default_markdown_body
 
-      File.write(content_file_path, default_markdown_body)
-
-      metadata_file      = 'index.yaml'
-      metadata_file_path = File.join(piece_path_to_create, metadata_file)
-      FileUtils.touch(metadata_file_path)
 
       default_yaml_metadata = <<YAML
 ---
 public: false
 
 YAML
+      piece.raw_yaml        = default_yaml_metadata
 
-      File.write(metadata_file_path, default_yaml_metadata)
+      piece.save
     end
 
     map n: :new
